df319becb35f6df111aad929f319dc091728bfdb
birdiebel <jp.bourdon.midam@gmail.com>
2026-02-11 15:52:33 +0100

    supprime vendor/bundle/ from git tracking

--- app/models/agecat.rb
+++ app/models/agecat.rb
@@ -2,6 +2,7 @@ class Agecat < ApplicationRecord
   has_and_belongs_to_many :playercats
   has_and_belongs_to_many :resultcats
 
+  # Ransackable attributes for searching and sorting
   def self.ransackable_attributes(auth_object = nil)
     [ "age_high", "age_low", "color", "created_at", "id", "name", "short", "updated_at", "year" ]
   end
