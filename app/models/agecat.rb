3c91eab29dbbd2d5b26c10b55ed4a1b0dd0a9feb
birdiebel <jp.bourdon.midam@gmail.com>
2026-02-11 15:58:13 +0100

    test commit

--- app/models/agecat.rb
+++ app/models/agecat.rb
@@ -3,6 +3,7 @@ class Agecat < ApplicationRecord
   has_and_belongs_to_many :resultcats
 
   # Ransackable attributes for searching and sorting
+  # This method is used by the Ransack gem to determine which attributes can be searched or sorted on.
   def self.ransackable_attributes(auth_object = nil)
     [ "age_high", "age_low", "color", "created_at", "id", "name", "short", "updated_at", "year" ]
   end
