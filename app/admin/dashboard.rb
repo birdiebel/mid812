aad2c33d4760b822af593fbfbd3af93117fc1938
birdiebel <jp.bourdon.midam@gmail.com>
2026-02-21 14:40:07 +0100

    event and round status part 1

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,33 +4,35 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    # div class: "blank_slate_container", id: "dashboard_default_message" do
-    #   span class: "blank_slate" do
-    #     span I18n.t("active_admin.dashboard_welcome.welcome")
-    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
-    #   end
-    # end
-
     columns do
+      # Colone de gauche
       column do
+        # Running events (running)
         panel "Running Events" do
           render "admin/dashboard/running_events"
         end
+        # Online events (online, registration, Waiting)
         panel "On Line Events" do
           render "admin/dashboard/online_events"
         end
+        # Future events (created)
         panel "Futures Events" do
           render "admin/dashboard/created_events"
         end
       end
-
+      # Colone de droite
       column do
+        # Infos
         panel "Info" do
           para class: "black is_bold" do
             "Infos"
           end
         end
+        # Terminated events (terminated, canceled)
+        panel "Terminated Events" do
+          render "admin/dashboard/terminated_events"
+        end
       end
     end
-  end # content
+  end
 end
